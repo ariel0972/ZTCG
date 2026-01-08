538fe6e2e8b8bce56a8f723f506bd6bcef079875
Ariel_EA <are3380@gmail.com>
2026-01-08 15:50:49 -0300

    REREnomeando

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const bcrypt = require("bcrypt")
 const jwt = require('jsonwebtoken')
 
 const { checkAdmin } = require('./middlewares/checkAdmin.js')
-const { checkToken } = require('./middlewares/temp.js')
+const { checkToken } = require('./middlewares/checkToken.js')
 
 const userRoutes = require('./routes/userRoutes');
 const adminRoutes = require('./routes/adminRoutes');
